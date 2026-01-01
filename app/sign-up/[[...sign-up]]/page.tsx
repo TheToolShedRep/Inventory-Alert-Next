a0dc6b80d85243e22114b85805bff6ab54c765e9
thetoolshedrep <fixed.my.ads@gmail.com>
2026-01-01 17:22:59 -0500

    debug google auth

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,16 +1,9 @@
-// app/sign-up/[[...sign-up]]/page.tsx
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div style={{ display: "grid", placeItems: "center", minHeight: "100vh" }}>
-      <SignUp
-        routing="path"
-        path="/sign-up"
-        signInUrl="/sign-in"
-        forceRedirectUrl="/manager"
-        fallbackRedirectUrl="/manager"
-      />
+      <SignUp />
     </div>
   );
 }
